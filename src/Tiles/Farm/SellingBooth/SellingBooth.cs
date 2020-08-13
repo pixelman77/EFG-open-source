0559e60aeedebae512d9340d773da2b88c650a23
GeoSTGames <55875553+GeoSTGames@users.noreply.github.com>
2020-08-14 00:25:08 +1000

    Fix indentation.

--- src/Tiles/Farm/SellingBooth/SellingBooth.cs
+++ src/Tiles/Farm/SellingBooth/SellingBooth.cs
@@ -5,26 +5,25 @@ using EvilFarmingGame.Tiles;
 
 public class SellingBooth : InteractableTile
 {
-	public Item HeldItem;
+    public Item HeldItem;
 
-	public override void _Ready()
-	{
-		InitTile();
-	}
+    public override void _Ready()
+    {
+	    InitTile();
+    }
 
-	public override void _PhysicsProcess(float delta)
-	{
-		if(PlayerColliding && Input.IsActionJustPressed("Player_Action") && PlayerBody.Inventory.HeldSlot < PlayerBody.Inventory.Items.Count)
-		{
-			HeldItem = PlayerBody.Inventory[PlayerBody.Inventory.HeldSlot];
+    public override void _PhysicsProcess(float delta)
+    {
+	    if(PlayerColliding && Input.IsActionJustPressed("Player_Action") && PlayerBody.Inventory.HeldSlot < PlayerBody.Inventory.Items.Count)
+	    {
+		    HeldItem = PlayerBody.Inventory[PlayerBody.Inventory.HeldSlot];
 
             if (HeldItem is Crop crop) {
                 PlayerBody.Currency += crop.Price;
                 PlayerBody.Inventory.Remove(HeldItem);
             }
         }
-		UpdateTile();
-	}
-
+	    UpdateTile();
+    }
 
 }
