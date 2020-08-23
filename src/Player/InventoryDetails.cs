a98a778bca075085e57faab4c0a01104f86e1783
pixelman77 <haghgoo33@gmail.com>
2020-08-23 13:19:58 +0430

    Adds the description mechanic

---
+++ src/Player/InventoryDetails.cs
@@ -0,0 +1,39 @@
+using Godot;
+using System;
+using EvilFarmingGame.Items;
+
+public class InventoryDetails : RichTextLabel
+{
+	public Player PlayerBody;
+	private bool Debugmode;
+	private Item HeldItem;
+	public override void _PhysicsProcess(float delta)
+	{
+		PlayerBody = GetParent().GetParent().GetParent<Player>();
+		if (PlayerBody.Inventory.Items.Count >= PlayerBody.Inventory.HeldSlot + 1)
+		{
+		HeldItem = PlayerBody.Inventory[PlayerBody.Inventory.HeldSlot];
+		}
+		else HeldItem = null;
+
+		Debugmode = GameControl.Debugging;
+		if (HeldItem != null)
+		{
+			this.Text = HeldItem.Name;
+			if (Debugmode)
+			{
+				this.Text += "\t\t" + HeldItem.ID;
+			}
+			if (Input.IsActionPressed("ui_show_des"))
+			{
+				this.Text += "\n" + HeldItem.Description;
+			}
+		}
+		else
+		{
+			this.Text = null;
+		}
+	}
+
+
+}
