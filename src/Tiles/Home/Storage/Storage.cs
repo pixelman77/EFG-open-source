fe857a53bb56d10ae3879e9cc57c103712d47519
ComLarsic <ComLarsic-mail@protonmail.com>
2020-09-15 17:55:05 +0200

    Add storage class that allows the creation of storagedevices.

--- src/Tiles/Home/Storage/Storage.cs
+++ src/Tiles/Home/Storage/Storage.cs
@@ -1,5 +1,6 @@
-
+using System;
 using EvilFarmingGame.Player;
+using Godot;
 
 namespace EvilFarmingGame.Tiles.Home.Storage
 {
@@ -7,18 +8,65 @@ namespace EvilFarmingGame.Tiles.Home.Storage
     {
         public Inventory InternalInventory;
 
-        public int Capacity = 10;
+        protected int Capacity = 10;
         
-        public void InitStorage()
+        protected void InitStorage()
         {
             InitTile();
             
             InternalInventory = new Inventory(Capacity);
         }
 
-        public void UpdateStorage()
+        protected void UpdateStorage()
         {
             UpdateTile();
+        }
+        
+        protected void InventoryHandling()
+        {
+            // Internal-InventoryHandling
+            for (int i = 0; i < InternalInventory.Items.Capacity; i++)
+            {
+                Sprite Icon = (Sprite) GetNode($"UI/Control/InventorySlots/InventorySlot{i + 1}/Item");
+                Sprite SelectBox = (Sprite) GetNode($"UI/Control/InventorySlots/InventorySlot{i + 1}/Selection");
+
+                if (InternalInventory.Items.Count <= i) Icon.Texture = (Texture) GD.Load("res://src/NoTexture.png");
+                else Icon.Texture = InternalInventory[i]?.Icon;
+            }
+            
+            // Player-InventoryHandling
+            for (int i = 0; i < PlayerBody?.Inventory.Items.Capacity; i++)
+            {
+                Sprite Icon = (Sprite) GetNode($"UI/Control/PlayerSlots/InventorySlot{i + 1}/Item");
+                Sprite SelectBox = (Sprite) GetNode($"UI/Control/PlayerSlots/InventorySlot{i + 1}/Selection");
+
+                if (PlayerBody.Inventory.Items.Count <= i) Icon.Texture = (Texture) GD.Load("res://src/NoTexture.png");
+                else Icon.Texture = PlayerBody.Inventory[i]?.Icon;
+            }
+        }
+        
+        
+        public void SlotClicked(InventorySlot slot)
+        {
+            if (slot.GetParent().Name == "PlayerSlots")
+            {
+                var slotIndex = Convert.ToInt32(slot.Name.Replace("InventorySlot", ""));
+                if (slotIndex <= PlayerBody.Inventory.Items.Count)
+                {
+                    InternalInventory.Gain(PlayerBody.Inventory[slotIndex -1]);
+                    PlayerBody.Inventory.Remove(PlayerBody.Inventory[slotIndex -1]);
+                }
+            }
+            else if (slot.GetParent().Name == "InventorySlots")
+            {
+                var slotIndex = Convert.ToInt32(slot.Name.Replace("InventorySlot", ""));
+                GD.Print(slotIndex);
+                if (slotIndex <= InternalInventory.Items.Count)
+                {
+                    PlayerBody.Inventory.Gain(InternalInventory[slotIndex -1]);
+                    InternalInventory.Remove(InternalInventory[slotIndex -1]);
+                }
+            }
         }
         
     }
--- src/Tiles/Home/Storage/TestStorage/TestCloset.cs
+++ src/Tiles/Home/Storage/TestStorage/TestCloset.cs
@@ -35,57 +35,7 @@ public class TestCloset : Storage
             if(PlayerBody != null) PlayerBody.CanMove = true;
         }
         
-        //TODO: Add UI for inventory
-        //foreach (var item in InternalInventory.Items)
-        //{
-        //    GD.Print(item?.Name);
-        //}
-        
         UpdateStorage();
     }
 
-    private void InventoryHandling()
-    {
-        // Internal-InventoryHandling
-        for (int i = 0; i < InternalInventory.Items.Capacity; i++)
-        {
-            Sprite Icon = (Sprite) GetNode($"UI/Control/InventorySlots/InventorySlot{i + 1}/Item");
-            Sprite SelectBox = (Sprite) GetNode($"UI/Control/InventorySlots/InventorySlot{i + 1}/Selection");
-
-            if (InternalInventory.Items.Count <= i) Icon.Texture = (Texture) GD.Load("res://src/NoTexture.png");
-            else Icon.Texture = InternalInventory[i]?.Icon;
-        }
-        // Player-InventoryHandling
-        for (int i = 0; i < PlayerBody?.Inventory.Items.Capacity; i++)
-        {
-            Sprite Icon = (Sprite) GetNode($"UI/Control/PlayerSlots/InventorySlot{i + 1}/Item");
-            Sprite SelectBox = (Sprite) GetNode($"UI/Control/PlayerSlots/InventorySlot{i + 1}/Selection");
-
-            if (PlayerBody.Inventory.Items.Count <= i) Icon.Texture = (Texture) GD.Load("res://src/NoTexture.png");
-            else Icon.Texture = PlayerBody.Inventory[i]?.Icon;
-        }
-    }
-
-    public void SlotClicked(InventorySlot slot)
-    {
-        if (slot.GetParent().Name == "PlayerSlots")
-        {
-            var slotIndex = Convert.ToInt32(slot.Name.Replace("InventorySlot", ""));
-            if (slotIndex <= PlayerBody.Inventory.Items.Count)
-            {
-                InternalInventory.Gain(PlayerBody.Inventory[slotIndex -1]);
-                PlayerBody.Inventory.Remove(PlayerBody.Inventory[slotIndex -1]);
-            }
-        }
-        else if (slot.GetParent().Name == "InventorySlots")
-        {
-            var slotIndex = Convert.ToInt32(slot.Name.Replace("InventorySlot", ""));
-            GD.Print(slotIndex);
-            if (slotIndex <= InternalInventory.Items.Count)
-            {
-                PlayerBody.Inventory.Gain(InternalInventory[slotIndex -1]);
-                InternalInventory.Remove(InternalInventory[slotIndex -1]);
-            }
-        }
-    }
 }
