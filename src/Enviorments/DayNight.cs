7024dfe0e7a6f1678d7136ff834b8279962dc344
ComLarsic <ComLarsic-mail@protonmail.com>
2020-11-21 22:28:29 +0100

    Add a new day-night cycle that works with the new Time-System

--- src/Enviorments/DayNight.cs
+++ src/Enviorments/DayNight.cs
@@ -4,16 +4,19 @@ using System;
 public class DayNight : Node2D
 {
     public AnimationPlayer AnimationPlayer;
-
+    
+    [Export()] public Gradient SkyGradient;
+    private CanvasModulate LightCanvas;
+    
     public bool Afternoon = false;
     public int Day = 1;
-    [Export] public int Hour = 0;
-    [Export] public int Minute = 0;
+    public int MinutesInDay = 0;
+    [Export()] public int Hour = 0;
+    [Export()] public int Minute = 0;
     
     public override void _Ready()
     {
-        //AnimationPlayer = (AnimationPlayer) GetNode("AnimationPlayer");
-        //AnimationPlayer.Play("Day-Night");
+        LightCanvas = GetNode<CanvasModulate>("CanvasModulate");
     }
 
     public override void _Process(float delta)
@@ -27,22 +30,27 @@ public class DayNight : Node2D
             Afternoon = true;
             Hour = 1;
         }
+        LightCanvas.Color = SkyGradient.Interpolate(MinutesInDay*(.0001f*7f));
     }
 
     private void NextMinute()
     {
         Minute++;
+        MinutesInDay++;
         if (Minute == 60)
         {
             Minute = 0;
             Hour++;
         }
+        
     }
 
     public void NextDay()
     {
         Afternoon = false;
         Hour = 0;
+        Minute = 0;
+        MinutesInDay = 0;
         Day++;
     }
 }
