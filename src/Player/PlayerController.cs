605aa61d7b3a7882db1427b7d8f10fe639f69644
ComLarsic <larsjanjensen@gmail.com>
2021-02-12 14:57:56 +0100

    Improve animation system, add direction enum in player-controller, add body-drag animation, update language version to C#-8, comment PlayerController.cs and Player.cs

--- src/Player/PlayerController.cs
+++ src/Player/PlayerController.cs
@@ -4,69 +4,90 @@ namespace EvilFarmingGame.Player
 {
 	public class PlayerController
 	{
-		private global::Player Body;
+		private global::Player Body; // Accessing the players body
 
+		// The players movement stats
 		[Export()] public float Speed = 75;
 		[Export()] public float SprintSpeed = 112;
 
 		[Export()] public float DraggingSpeed = 50f;
 		[Export()] public float SlowerDraggingSpeed = 25f;
 
+		public enum Direction // For determining the direction the player is facing
+		{
+			Up=0, Down, Left, Right
+		}
+
+		public Direction CurrentDirection; // THe players current facing direction
+
 		public PlayerController(global::Player Body)
 		{
 			this.Body = Body;
 		}
-
-		public Vector2 InputMovement(float delta) // The Movement method based on Input 
+	
+		// The Movement method based on Input 
+		public Vector2 InputMovement(float delta)
 		{
 			Vector2 velocity = new Vector2();
-
+			
+			// Determine the players movement direction and the facing direction
 			if (Input.IsActionPressed("Player_Up"))
 			{
 				velocity += Vector2.Up;
-				Body.RayPivot.RotationDegrees = 180;
+				CurrentDirection = Direction.Up;
 			}
 			else if (Input.IsActionPressed("Player_Down"))
 			{
 				velocity += Vector2.Down;
-				Body.RayPivot.RotationDegrees = 0;
+				CurrentDirection = Direction.Down;
 			}
-			
+
 			if (Input.IsActionPressed("Player_Left"))
 			{
 				velocity += Vector2.Left;
-				Body.RayPivot.RotationDegrees = 90;
+				CurrentDirection = Direction.Left;
 			}
 			else if (Input.IsActionPressed("Player_Right"))
 			{
 				velocity += Vector2.Right;
-				Body.RayPivot.RotationDegrees = 270;
+				CurrentDirection = Direction.Right;
 			}
-
-			if (velocity.Length() > 1)
+			
+			if (velocity.Length() > 1) velocity = velocity.Normalized(); // Normalize the players velocity
+			
+			// Determine movement speed
+			if (Body.IsDragging) // Drag the body
 			{
-				velocity.Normalized();
-			}
-
-			if (Input.IsActionPressed("Player_Sprint") && Body.Stamina > 0 && !Body.IsDragging)
-			{
-				velocity *= SprintSpeed;
-			}
-			else if (Body.IsDragging)
-			{
-				if (Body.Stamina > 0)
+				if (Body.Stamina > 0) // Apply a faster dragging speed if the player has enough stamina
 				{
 					velocity *= DraggingSpeed;
-					Body.Stamina -= 5f * delta;
+					Body.Stamina -= 5f * delta; // Decrease the players stamina 
 				}
-				else velocity *= SlowerDraggingSpeed;
+				else velocity *= SlowerDraggingSpeed; // Apply a slower dragging speed when the stamina is 0
+				
+				// Determine the direction based on the dragged bodies angle relative to the player
+				var angle = Mathf.Rad2Deg(Body.Position.AngleToPoint(Body.DragginBody.Position)); // Determine the bodies angle relative to the player
+				if (angle > -45 && angle < 45) CurrentDirection = Direction.Left; // Direction left
+				else if (angle > 45 && angle < 135) CurrentDirection = Direction.Up; // Direction up
+				else if (angle > -135 && angle < 135) CurrentDirection = Direction.Down; // Direction Down
+				else CurrentDirection = Direction.Right; // Direction right
 			}
+			else if (Input.IsActionPressed("Player_Sprint") && Body.Stamina > 0) // Apply sprint speed is the player is sprinting and the player has enough stamina 
+				velocity *= SprintSpeed;
 			else
+				velocity *= Speed; // Apply regular waling speed
+			
+			// Rotate body
+			Body.RayPivot.RotationDegrees = CurrentDirection switch
 			{
-				velocity *= Speed;
-			}
-
-			return Body.MoveAndSlide(velocity);
+				Direction.Up => 180,
+				Direction.Down => 0,
+				Direction.Left => 90,
+				Direction.Right => 270,
+				_=> 0
+			};
+			
+			return Body.MoveAndSlide(velocity); // Apply the players velocity velocity and return the result
 		}
 
 	}
